506d1eaac70275ac5756d44b4db5353221bea65b
Aneurin Price <aneurinprice@handles.local>
2022-11-27 17:07:53 +0000

    v0.0.1

--- main.py
+++ main.py
@@ -2,18 +2,63 @@ from plexapi.server import PlexServer
 import os
 import requests
 import shutil
+import yaml
+import re
 
-PlexHost = 'PLEX HOST HERE'
-PlexToken = 'PLEX TOKEN HERE'
-PlexMoviesLibrary = "NAME OF MOVIE LIBRARY"
+def LoadConfig():
+	with open('config.yaml', 'r') as file:
+	    config = yaml.safe_load(file)
+	plex = PlexServer(config['PlexHost'], config['PlexToken'])
+	plex.resources()
+	return config, plex
 
-plex = PlexServer(PlexHost, PlexToken)
+def DownloadPoster (movie):
+	Poster = re.sub("[^a-zA-Z0-9]+", "",movie.title)
+	Poster = Poster + '.jpg'
+	result = requests.get(config['PlexHost']+movie.thumb, headers={'X-PLEX-TOKEN': config['PlexToken']}, stream = True)
+	result.decode_content = True
+	with open('tmp/' + Poster,'wb') as f:
+		shutil.copyfileobj(result.raw, f)
+	return Poster
 
-movies = plex.library.section(PlexMoviesLibrary)
-movies = movies.search(resolution=2160)
-# Film Index number here --------------| I will fix this at some point
-result = requests.get(PlexHost+movies[122].thumb, headers={'X-PLEX-TOKEN': PlexToken}, stream = True)
-result.decode_content = True
-with open("image.jpg",'wb') as f:
-	shutil.copyfileobj(result.raw, f)
-	os.system('magick image.jpg assets/UHD.png -resize %[fx:u.w]x%[fx:u.h] -gravity north -composite output.jpg')
+def AddBanner (movie, category, Poster):
+	os.system('magick tmp/' + Poster + ' assets/' + category + '.png -resize %[fx:u.w]x%[fx:u.h] -gravity north -composite output/' + Poster)
+	
+
+def UploadPoster (movie, Poster):
+	movie.uploadPoster(filepath='./output/' + Poster)	
+		
+def TidyUp (movie, Poster):
+	if not (config['KeepPoster']):
+		os.remove('output/' + Poster) 
+	os.remove('tmp/' + Poster)
+
+def RunMain (movie, category):
+	Poster = DownloadPoster(movie)
+	AddBanner(movie, category, Poster)
+	if (config['UploadPoster']):
+		UploadPoster(movie, Poster)
+	TidyUp(movie, Poster)
+	print("Completed " + movie.title)
+
+
+
+
+
+	
+config, plex = LoadConfig()
+movies = plex.library.section(config['PlexMoviesLibrary'])
+
+
+if config['HandleHDR']:
+	for movie in movies.search(resolution="4k", hdr=False):
+		RunMain(movie, "UHD")
+	for movie in movies.search(resolution="4k", hdr=True):
+		RunMain(movie, "HDR")
+
+if not config['HandleHDR']:
+	for movie in movies.search(resolution="4k"):
+		RunMain(movie, "UHD")
+
+for movie in movies.search(resolution="1080p"):
+	RunMain(movie, "Bluray")
